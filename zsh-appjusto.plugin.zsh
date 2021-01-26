70fb44deb485a7b27c89133cffbe5cc94770a5ee
Pedro Andrade <pdandradeb@gmail.com>
2021-01-26 10:13:26 -0300

    Fixing login

--- zsh-appjusto.plugin.zsh
+++ zsh-appjusto.plugin.zsh
@@ -10,8 +10,14 @@ alias aj-app-courier="FLAVOR=courier FIREBASE_EMULATOR=true expo start -c"
 alias aj-app-courier-live="FLAVOR=courier expo start"
 alias aj-app-consumer="FLAVOR=consumer FIREBASE_EMULATOR=true expo start -c"
 alias aj-app-consumer-live="FLAVOR=consumer expo start"
-alias aj-app-login-ios="xcrun simctl openurl booted \"exp://$AJ_LOCAL_IP:19000/--/?link=$AJ_LINK\""
-alias aj-app-login-android="adb shell am start -a \"android.intent.action.VIEW\" -d \"exp://$AJ_IP:19000/--/?link=$AJ_LINK\""
+function aj-app-login-ios() {
+  local address="$1" link="$2"
+  xcrun simctl openurl booted "exp://$address/--/?link=$link$"
+}
+function aj-app-login-android() {
+  local address="$1" link="$2"
+  adb shell am start -a "android.intent.action.VIEW" -d "exp://$address/--/?link=$link"
+}
 alias aj-app-clear-cache="watchman watch-del-all && rm -rf node_modules/ && rm -f package-lock.json && npm install"
 alias aj-app-sync="rsync -va --delete $APPJUSTO_DIR/app/ $APPJUSTO_DIR/app-clone/ --exclude=\".expo/\" --exclude=\".git/*\""
 
