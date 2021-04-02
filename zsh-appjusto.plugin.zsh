1d40fb4583e4a5c21e4f7a167a84835a8527bff7
Pedro Andrade <pdandradeb@gmail.com>
2021-04-02 09:43:19 -0300

    Starting firebase functions as we open the tab

--- zsh-appjusto.plugin.zsh
+++ zsh-appjusto.plugin.zsh
@@ -1,7 +1,7 @@
 # general
 alias aj-cd="cd $APPJUSTO_DIR"
 # TODO: tmux
-alias aj-session="aj-cd-admin && split_tab 'aj-cd-admin; aj-admin-start' && split_tab 'aj-cd-app; aj-app-consumer' && split_tab aj-cd-app-clone && split_tab aj-cd && split_tab 'aj-cd-fb' && split_tab aj-cd-fb && split_tab aj-cd-types"
+alias aj-session="aj-cd-admin && split_tab 'aj-cd-admin; aj-admin-start' && split_tab 'aj-cd-app; aj-app-consumer' && split_tab aj-cd-app-clone && split_tab aj-cd && split_tab 'aj-cd-fb; aj-fb-start' && split_tab aj-cd-fb && split_tab aj-cd-types"
 
 # app
 alias aj-cd-app="aj-cd && cd ./app"
@@ -9,10 +9,12 @@ alias aj-cd-app-clone="aj-cd && cd ./app-clone"
 alias aj-app-dev="cp .dev.env .env"
 alias aj-app-staging="cp .staging.env .env"
 alias aj-app-live="cp .live.env .env"
-alias aj-app-courier="aj-app-dev && && FLAVOR=courier FIREBASE_EMULATOR=true expo start -c"
+alias aj-app-courier="aj-app-dev && && FIREBASE_EMULATOR=true FLAVOR=courier expo start -c"
+alias aj-app-courier-dev="aj-app-dev && && FLAVOR=courier expo start -c"
 alias aj-app-courier-staging="aj-app-staging && FLAVOR=courier expo start"
 alias aj-app-courier-live="aj-app-live && FLAVOR=courier expo start"
-alias aj-app-consumer="aj-app-dev && FLAVOR=consumer FIREBASE_EMULATOR=true expo start -c"
+alias aj-app-consumer="aj-app-dev && FIREBASE_EMULATOR=true FLAVOR=consumer expo start -c"
+alias aj-app-consumer-dev="aj-app-dev && FLAVOR=consumer expo start -c"
 alias aj-app-consumer-staging="aj-app-staging && FLAVOR=consumer expo start"
 alias aj-app-consumer-live="aj-app-live && FLAVOR=consumer expo start"
 function aj-app-login-ios() {
