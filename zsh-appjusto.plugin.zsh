ff6eea5eb70c59f2fb65079efb83f8be3f56c948
Pedro Andrade <pdandradeb@gmail.com>
2021-04-27 10:08:09 -0300

    Fixing firebase emulator commands

--- zsh-appjusto.plugin.zsh
+++ zsh-appjusto.plugin.zsh
@@ -9,11 +9,11 @@ alias aj-cd-app-clone="aj-cd && cd ./app-clone"
 alias aj-app-dev="cp .dev.env .env"
 alias aj-app-staging="cp .staging.env .env"
 alias aj-app-live="cp .live.env .env"
-alias aj-app-courier="aj-app-dev && FIREBASE_EMULATOR=true && FLAVOR=courier expo start -c"
+alias aj-app-courier="aj-app-dev && FIREBASE_EMULATOR=true FLAVOR=courier expo start -c"
 alias aj-app-courier-dev="aj-app-dev && FLAVOR=courier expo start -c"
 alias aj-app-courier-staging="aj-app-staging && FLAVOR=courier expo start"
 alias aj-app-courier-live="aj-app-live && FLAVOR=courier expo start"
-alias aj-app-consumer="aj-app-dev && FIREBASE_EMULATOR=true && FLAVOR=consumer expo start -c"
+alias aj-app-consumer="aj-app-dev && FIREBASE_EMULATOR=true FLAVOR=consumer expo start -c"
 alias aj-app-consumer-dev="aj-app-dev && FLAVOR=consumer expo start -c"
 alias aj-app-consumer-staging="aj-app-staging && FLAVOR=consumer expo start"
 alias aj-app-consumer-live="aj-app-live && FLAVOR=consumer expo start"
@@ -30,9 +30,9 @@ alias aj-app-sync="rsync -va --delete $APPJUSTO_DIR/app/ $APPJUSTO_DIR/app-clone
 
 # firebase-functions
 alias aj-cd-fb="aj-cd && cd ./firebase-functions/functions"
-alias aj-fb-start="firebase emulators:start --import=$APPJUSTO_DIR/firestore-data/current --export-on-exit"
-alias aj-fb-debug="firebase emulators:start --import=$APPJUSTO_DIR/firestore-data/current --export-on-exit --inspect-functions"
-alias aj-fb-update-config="firebase functions:config:get > .runtimeconfig.json"
+alias aj-fb-start="firebase use app-justo-dev && firebase emulators:start --import=$APPJUSTO_DIR/firestore-data/current --export-on-exit"
+alias aj-fb-debug="firebase use app-justo-dev firebase emulators:start --import=$APPJUSTO_DIR/firestore-data/current --export-on-exit --inspect-functions"
+alias aj-fb-update-config="firebase use app-justo-dev && firebase functions:config:get > .runtimeconfig.json"
 function aj-fb-deploy-function() {
   local name="$1"
   npm run install-remote-types && firebase deploy --only functions:$name && npm run install-local-types
